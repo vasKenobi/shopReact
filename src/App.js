8aca6f8379fa0e9e463652b486ab513cb0d7d890
VasLets <vasyl.klets@gmail.com>
2019-11-13 21:23:38 +0200

    created basic template in App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
-import React from 'react';
+import React from 'react'
 
-function App(){
+import './reset.css'
+import './base.css'
+import './header.css'
+import './menu.css'
+import './cart.css'
+
+const App = () => {
 	return (
-    <div>
-      <h1>Hello</h1>
-      <p>Hello</p>
-    </div>
-    )
-  
+		<div>
+			<header class="header">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-3">
+							<div class="logo">
+								<img src="img/logo.png" alt="" />
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="menu">
+								<ul>
+									<li><a href="">Home</a></li>
+									<li><a href="">Products</a></li>
+									<li><a href="">Payment</a></li>
+									<li><a href="">Shipping</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-3">
+							<div class="cart text-center">
+								<div class="products-count">0</div>
+								<div class="products-price">$500</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+			<main class="main">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-3">
+							
+						</div>
+						<div class="col-lg-9">
+							
+						</div>
+					</div>
+				</div>
+			</main>
+			<footer class="footer">
+				
+			</footer>
+		</div>
+	)
 }
 
 
-export default App;
+export default App
