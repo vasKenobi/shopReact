75355d3f0752ac9efcecd49c0e545ca832f102b6
VasLets <vasyl.klets@gmail.com>
2019-11-14 21:24:06 +0200

    created Header,Main,Footer components

--- src/App.js
+++ src/App.js
@@ -6,57 +6,72 @@ import './header.css'
 import './menu.css'
 import './cart.css'
 
+const Header = () => {
+	return (
+		<header className="header">
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-3">
+						<div className="logo">
+							<img src="img/logo.png" alt="" />
+						</div>
+					</div>
+
+					<div className="col-lg-6">
+						<div className="menu">
+							<ul>
+								<li><a href="/">Home</a></li>
+								<li><a href="/">Products</a></li>
+								<li><a href="/">Payment</a></li>
+								<li><a href="/">Shipping</a></li>
+							</ul>
+						</div>
+					</div>
+
+					<div className="col-lg-3">
+						<div className="cart text-center">
+							<div className="products-count">0</div>
+							<div className="products-price">$500</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+	)
+}
+
+const Main = () => {
+	return (
+		<main className="main">
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-3">
+						Filter
+					</div>
+					<div className="col-lg-9">
+						Products List
+					</div>
+				</div>
+			</div>
+		</main>
+	)
+}
+
+const Footer = () => {
+	return (
+		<footer className="footer">
+			Footer		
+		</footer>
+	)
+}
+
 const App = () => {
 	return (
 		<div>
-			<header className="header">
-				<div className="container">
-					<div className="row">
-						<div className="col-lg-3">
-							<div className="logo">
-								<img src="img/logo.png" alt="" />
-							</div>
-						</div>
-
-						<div className="col-lg-6">
-							<div className="menu">
-								<ul>
-								<li><a href="/">Home</a></li>
-									<li><a href="/">Products</a></li>
-									<li><a href="/">Payment</a></li>
-									<li><a href="/">Shipping</a></li>
-								</ul>
-							</div>
-						</div>
-						<div className="col-lg-3">
-							<div className="cart text-center">
-								<div className="products-count">0</div>
-								<div className="products-price">$500</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</header>
-
-			<main className="main">
-				<div className="container">
-					<div className="row">
-						<div className="col-lg-3">
-							
-						</div>
-						<div className="col-lg-9">
-							
-						</div>
-					</div>
-				</div>
-			</main>
-
-			<footer className="footer">
-				
-			</footer>
+			<Header/>
+			<Main/>
+			<Footer/>
 		</div>
-
-
 	)
 }
 
