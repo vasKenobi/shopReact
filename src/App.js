129166f6e7e86affafbf56e6b4a9fd9eb660e61f
VasLets <vasyl.klets@gmail.com>
2019-11-14 21:50:47 +0200

    added Header,Main,Footer files

--- src/App.js
+++ src/App.js
@@ -6,64 +6,9 @@ import './header.css'
 import './menu.css'
 import './cart.css'
 
-const Header = () => {
-	return (
-		<header className="header">
-			<div className="container">
-				<div className="row">
-					<div className="col-lg-3">
-						<div className="logo">
-							<img src="img/logo.png" alt="" />
-						</div>
-					</div>
-
-					<div className="col-lg-6">
-						<div className="menu">
-							<ul>
-								<li><a href="/">Home</a></li>
-								<li><a href="/">Products</a></li>
-								<li><a href="/">Payment</a></li>
-								<li><a href="/">Shipping</a></li>
-							</ul>
-						</div>
-					</div>
-
-					<div className="col-lg-3">
-						<div className="cart text-center">
-							<div className="products-count">0</div>
-							<div className="products-price">$500</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</header>
-	)
-}
-
-const Main = () => {
-	return (
-		<main className="main">
-			<div className="container">
-				<div className="row">
-					<div className="col-lg-3">
-						Filter
-					</div>
-					<div className="col-lg-9">
-						Products List
-					</div>
-				</div>
-			</div>
-		</main>
-	)
-}
-
-const Footer = () => {
-	return (
-		<footer className="footer">
-			Footer		
-		</footer>
-	)
-}
+import Header from './Header.js'
+import Main from './Main.js'
+import Footer from './Footer.js'
 
 const App = () => {
 	return (
---
+++ src/Main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Main = () => {
+	return (
+		<main className="main">
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-3">
+						Filter
+					</div>
+					<div className="col-lg-9">
+						Products List
+					</div>
+				</div>
+			</div>
+		</main>
+	)
+}
+
+export default Main
