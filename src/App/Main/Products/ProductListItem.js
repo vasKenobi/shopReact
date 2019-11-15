9f0ce1d38b9e0ee7ba6a6f6319ee1ff172c33869
VasLets <vasyl.klets@gmail.com>
2019-11-15 23:16:16 +0200

    connected styles for ProductListItem component

---
+++ src/App/Main/Products/ProductListItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './ProductListItem.css'
+
+const ProductListItem = () => {
+    return (
+        <div className="product-list-item">
+            <div className="product-title">iPhone 11</div>
+            <div className="product-description">This is iPhone 11</div>
+            <div className="product-features">Type: phone</div>
+            <div className="product-features">Capacity: 64 Gb</div>
+            <div className="product-price">Price : 500$</div>
+            <button className="btn btn-add-to-cart">Add to cart</button>
+
+        </div>
+    )
+}
+
+export default ProductListItem
--- src/App/Main/Products/ProductsList.js
+++ src/App/Main/Products/ProductsList.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import ProductListItem from './ProductListItem'
 
 const ProductsList = () => {
     return (
         <div>
             <h1 className="page-title">Products List</h1>
+            <div className="row">
+                <div className="col-lg-6">
+                    <ProductListItem/>
+                </div>
+            </div>
+
 
         </div>
     )
