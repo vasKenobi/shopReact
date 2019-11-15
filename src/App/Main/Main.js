32824fbfa1579b066aa2b758895fb2fb0299621e
VasLets <vasyl.klets@gmail.com>
2019-11-15 23:08:29 +0200

    added ProductsList component to Main

--- src/App/Main/Main.js
+++ src/App/Main/Main.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ProductsList from './Products/ProductsList'
 
 
 const Main = () => {
@@ -10,7 +11,7 @@ const Main = () => {
 						Filter
 					</div>
 					<div className="col-lg-9">
-						Products List
+						<ProductsList/>
 					</div>
 				</div>
 			</div>
---
+++ src/App/Main/Products/ProductsList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductsList = () => {
+    return (
+        <div>
+            <h1 className="page-title">Products List</h1>
+
+        </div>
+    )
+}
+
+export default ProductsList
